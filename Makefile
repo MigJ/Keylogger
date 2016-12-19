0b3e0b81b4a6e5ed9b6b265fe8d29ef96f545b71
Tréhout <nathan.trehout@epitech.eu>
2016-12-19 16:51:30 +0100

    First upload

---
+++ Makefile
@@ -0,0 +1,27 @@
+CC	=	gcc -g3
+
+NAME	=	Keylogger
+
+SRC	=	*.c
+
+LIB	=
+
+CFLAGS	=	-I./includes/ -W -Wall -Wextra -pedantic
+
+RM	=	rm -fr
+
+all	:	$(NAME)
+
+$(NAME)	:
+
+		$(CC) -o $(NAME) $(SRC) $(CFLAGS) -L lib -lmy
+
+clean	:
+
+		$(RM) $(OBJ)
+
+fclean	:	clean
+
+		$(RM) $(NAME)
+
+re	:	fclean all
---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+** main.c for keylogger in /home/azword/delivery/Perso/PSU/Keylogger
+** 
+** Made by Nathan Tréhout | Az'
+** Login   <nathan.trehout@epitech.eu>
+** 
+** Started on  Mon Dec 19 16:49:34 2016 Nathan Trehout
+** Last update Mon Dec 19 16:50:41 2016 Nathan Trehout
+*/
+
+
+
+int	main(int ac, char **av)
+{
+
+  return (0);
+}
